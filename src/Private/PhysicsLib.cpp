3a2f68b3d8827efc3440a4cde034ce2f4dde94ff
lmsorenson <lsorenson20@gmail.com>
2021-11-08 20:49:35 -0600

    add tests for freefall

--- src/Private/PhysicsLib.cpp
+++ src/Private/PhysicsLib.cpp
@@ -3,7 +3,8 @@
 
 double free_fall_distance(double t, Velocity v, double a)
 {
-    /// Calculate distance
+    /// Calculate distance -
+    /// d = vi * t + 1/2 * a * t^2
     double d = v.direction.z * t + 0.5 * a * pow(t, 2);
 
     return d;
@@ -12,6 +13,7 @@ double free_fall_distance(double t, Velocity v, double a)
 double velocity_at_time_interval(double t, Velocity v, double a)
 {
     /// Calculate final velocity.
+    /// vf = vi + a * t
     double vF = v.direction.z + a * t;
 
     return vF;
